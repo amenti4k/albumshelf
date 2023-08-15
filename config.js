362335bc638c26299a0059b227ad570874f8a972
Amenti Kenea <55615576+amenti4k@users.noreply.github.com>
2023-08-14 19:28:28 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-
+module.exports = {
+    clientId: '029941bb0f4b47768c899b25819fa09d',
+    clientSecret: '46374e728c21409d9211ad790d1aa69d',
+    redirectUri: 'http://localhost:3000/callback' // Change this for production
+};
